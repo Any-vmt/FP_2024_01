63934b6bce02e48dfd47993a8f2d9bede7cfaf81
Any-vmt <anyorpa@gmail.com>
2024-10-04 12:52:05 -0500

    Agregando descripción

--- Semana02.py
+++ Semana02.py
@@ -2,3 +2,6 @@ import streamlit as st
 import pandas as pa
 #Titulo de la aplicación.
 st.title("Introducción a variables y tipos de datos en python")
+
+#Descripción Inicial
+st.write("Python es un lenguaje de programación dinámico donde")
