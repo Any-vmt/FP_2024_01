168c2665eacab340765245940f95d5ae391903dd
Any-vmt <anyorpa@gmail.com>
2024-10-05 00:13:20 -0500

    creando 1era descripcion

--- Test01.py
+++ Test01.py
@@ -1,3 +1,6 @@
 import streamlit as st
 #Titulo de la Aplicación
 st.title("Introducción a variables y tipo de datos en Python")
+
+#Creando 1era descripción inicial
+st.write("Python es un lenguaje de programacion dinámico donde...")
