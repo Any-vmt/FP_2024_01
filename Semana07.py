1125c96c749a9d57e0614ad9a3b7ec8376ca984e
Any-vmt <anyorpa@gmail.com>
2024-10-24 21:58:16 -0500

    Reemplazando 02

--- Semana07.py
+++ Semana07.py
@@ -1,26 +1,26 @@
 import streamlit as st
 
 def verificar_contrasena():
-    st.title("Verificación de contraseña")
+    st.title("Verificación de contraseña")
 
-    # Contraseña correcta
-    contrasena_correcta = "asdasd"
+    # Contraseña correcta
+    contrasena_correcta = "asdasd"
 
-    # Inicializar variable de sesión para almacenar la entrada del usuario
-    if "contrasena_ingresada" not in st.session_state:
-        st.session_state.contrasena_ingresada = ""
+    # Inicializar variable de sesión para almacenar la entrada del usuario
+    if "contrasena_ingresada" not in st.session_state:
+        st.session_state.contrasena_ingresada = ""
 
-    # Input de la contraseña
-    contrasena_ingresada = st.text_input("Introduce la contraseña:", type="password")
+    # Input de la contraseña
+    contrasena_ingresada = st.text_input("Introduce la contraseña:", type="password")
 
-    # Botón para verificar la contraseña
-    if st.button("Verificar"):
-        if contrasena_ingresada == contrasena_correcta:
-            st.session_state.contrasena_ingresada = contrasena_ingresada
-            st.success("Bienvenido")
-        else:
-            st.error("Contraseña incorrecta, intenta de nuevo")
+    # Botón para verificar la contraseña
+    if st.button("Verificar"):
+        if contrasena_ingresada == contrasena_correcta:
+            st.session_state.contrasena_ingresada = contrasena_ingresada
+            st.success("Bienvenido")
+        else:
+            st.error("Contraseña incorrecta, intenta de nuevo")
 
 # Llamada a la función principal
-if _name_ == "_main_":
-    verificar_contrasena()
+if __name__ == "__main__":
+    verificar_contrasena()
