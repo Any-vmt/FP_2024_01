dfc53e312e1e9917ea22448c55bd859fb6675ae1
Any-vmt <anyorpa@gmail.com>
2024-10-24 21:41:41 -0500

    Contrasena sin while

--- Semana06.py
+++ Semana06.py
@@ -22,4 +22,36 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 if st.button("Mostrar números pares entre 0 y 100"):
-    pares0a100={}
+    pares0a100=[i for i in range (0,101) if i % 2==0]
+    st.write("Número pares entre 0 y 100:")
+    st.write(pares0a100)
+
+
+
+    st.write("###########################################")
+    #Funcion para generar la serie numérica.
+    def generar_serie(n):
+        serie = []
+        elementos = [1,5,3,7,5,9,7]
+        for i in range(n):
+            serie.append(elementos[i%len(elementos)])
+            return serie
+    #Funcion para validar y calcular la suma
+    def calcular_suma_serie(n):
+        if n <=0:
+            return "El valor de n debe ser mayor que 0"
+        serie = generar_serie(n)
+        suma = sum(serie)
+        return suma, serie
+
+    #Streamlit para obtener el valor de n
+    st.title("Ejercicio06: Suma de los n elementos de la serie")
+
+    #Ingresar un número por teclado
+    n= st.number_input("ingrese el valor de n:", min_value=1, step=1 )
+    
+    #Boton para calcular la suma
+    if st.button("Calcular suma"):
+        suma, serie = calcular_suma_serie(n)
+        st.write(f"La serie generada para n={n} es:{serie}")
+        st.write(f"La suma de los primeros {n}elementos es: {suma}")
---
+++ Semana07.py
@@ -0,0 +1,28 @@
+import streamlit as streamlit
+
+def verificar_contrasena():
+    st.title("Verificación de contraseña")
+
+
+    #Variable para alamacenar la contraseña
+    contrasena_correcta = "asdasd"
+    contrasena_ingresada = ""
+
+    #Inicializar variable de sesión para almacenar la entrada del usuario
+    if "Conraseña_ingresada" not in st.sesion_state:
+        st.session_state.contrasena_ingresada = ""
+    
+    #Input de la contraseña
+    contrasena_ingresada =st.text_input("Ingrese la constraseña", type="password")
+
+    #Boton para verificar la contraseña
+    if st.button("Verificar"):
+        if contrasena_ingresada == contrasena_correcta:
+            st.session_state.contrasena_ingresada = contrasena_ingresada
+            st.success("Bienvenido")
+        else:
+            st.error("Contrasena incorrecta, intente de nuevo")
+
+    #Llamada a la función principal
+    if __name__ == "__main__":
+        verificar_contrasena()
