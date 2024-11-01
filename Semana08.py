8c17d25f23086728c2db6bf3b477d0256d127e3d
Any-vmt <anyorpa@gmail.com>
2024-10-31 21:35:33 -0500

    add 2 puntos

--- Semana08.py
+++ Semana08.py
@@ -1,9 +1,10 @@
 import streamlit as st
 import math
+
 def calcular_area (area):
     return math.pi*(radio**2) 
 
-def calcular_perimetro (radio)
+def calcular_perimetro(radio):
     return 2*math.pi*radio
 
 def main():
